ce3955cd03cceb04e9c04673664fd17b3555241b
Vilhelm Melkstam <vilhelm.melkstam@itggot.se>
2016-04-27 12:40:43 +0200

    Login, register, log out now works! YAY

--- app.rb
+++ app.rb
@@ -2,24 +2,47 @@ class App < Sinatra::Base
   enable :sessions
 
   get '/' do
-  	slim :login, layout: false
+		if session[:user_id]
+			@user = User.get(session[:user_id])
+			redirect "/issues"
+		else
+  		slim :login, layout: false
+		end
   end
 
 	get '/issues' do
-		@issues = Issue.all
-		slim :issues
+		if session[:user_id]
+			@user = User.get(session[:user_id])
+			@issues = Issue.all(user_id: @user.id)
+			slim :issues
+		else
+			redirect "/"
+		end
 	end
 
 	post "/logout" do
+		session[:user_id] = nil
 		redirect "/"
 	end
 
   post "/login" do
-
+		user = User.first(email: params["email"])
+    if user && user.password == params["password"]
+      session[:user_id] = user.id
+    end
+    redirect "/issues"
   end
 
   post "/register" do
-    
+		if params["password"] == params["password-repeat"]
+			user = User.create(name: params["name"],
+												 email: params["email"],
+												 password: params["password"])
+		end
+    if user
+      session[:user_id] = user.id
+    end
+    redirect "/issues"
   end
 
 
