97f6a38c98d14cd3d1b4fd6f4fff014cfb26b1fe
Vilhelm Melkstam <vilhelm.melkstam@itggot.se>
2016-05-07 23:43:27 +0200

    Just testing around with snackbars
    Ain't working

--- public/js/snackbar.js
+++
@@ -1,7 +0,0 @@
-var snackbar = document.querySelector('#snackbar');
-var flashes = document.querySelector('#details').innerHTML.split("¤").filter(Boolean);
-console.log(flashes);
-console.log(flashes.length);
-for (var i = 0; i < flashes.length; i++){
-  snackbar.MaterialSnackbar.showSnackbar({message: flashes[i]});
-}
