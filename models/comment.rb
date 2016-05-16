39318985786127b16e3920d81bf886ab215db83f
Vilhelm Melkstam <vilhelm.melkstam@itggot.se>
2016-05-16 11:22:31 +0200

    Comments model added

--- models/comment.rb
+++ models/comment.rb
@@ -1,4 +1,11 @@
-# class Comment
-# 	include DataMapper::Resource
-#
-# end
+class Comment
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :description, Text, required: true
+	property :status, Enum[:comment, :status_closed, :question, :information], default: :comment
+	property :created_at, DateTime
+
+	belongs_to :user
+	belongs_to :issue
+end
